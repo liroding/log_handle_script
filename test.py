c738d221852b399a8f4b4511eb8fd6f9522e8541
liroding <liroding@126.com>
2019-03-07 19:37:31 +0800

    second
    Signed-off-by: liroding <liroding@126.com>

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ from lib2to3.fixer_util import String
 class HandleLogClass:
     
     ''' log name '''
-    logname = "/debuglog.log"
+    logname = "/11.log"
    
             
     def enter(self):
@@ -53,7 +53,7 @@ class HandleLogClass:
         _cmd  = '';
         
         for line in fd.readlines():                      
-            print line      #debug
+            #print line      #debug
             if 'Read' in line or 'Write' in line :
                  
                 wxlist= re.split(r',',line)   #wxlist is list format data
@@ -62,7 +62,7 @@ class HandleLogClass:
                     _str = wxlist[index]      #addr=0x111  
        
                     if 'offset' in _str:
-                         print 'offset'
+                        # print 'offset'
                          _local = _str.find('=')   
                          str_value = _str[_local+1:len(_str)]    #0x
                          _addr_value = str_value
@@ -72,45 +72,71 @@ class HandleLogClass:
                          elif 'Write' in _str:
                              _cmd = "write" 
                     elif 'data' in _str:
-                         print 'data'
+                        # print 'data'
                          _local = _str.find('=')   
                          str_value = _str[_local+1:len(_str)]    #0x
                          _data_value = str_value
                     elif 'size' in _str:
-                         print 'size'
+                        # print 'size'
                          _local = _str.find('=')   
                          str_value = _str[_local+1:len(_str)-1]    #0x
                          _len_value = str_value
                     else:
                          pass
                   
-                print _addr_value , _data_value ,_len_value ,_cmd
+                #print _addr_value , _data_value ,_len_value ,_cmd      #debug
             
-                self.json_handle(_addr_value, _data_value,_len_value, _cmd)      
+                self.json_handle(_addr_value, _data_value,_len_value, _cmd ,'invaild')      
             
             else:
-                 print "no write/read line" 
+                 pass
+                 #print "no write/read line"     #debug add
         fd.close()
  #   def scan_line_data(self):
-    JsonPacket = {'offset': '0' ,'data': '0', 'size' : '0' ,'cmd' : '0'}
+    JsonPacket = {'offset': '0' ,'data': '0', 'size' : '0' ,'cmd' : '0','item' : 'invaild'}
     
-    def json_handle(self,offset,data,size,cmd):
+    def json_handle(self,offset,data,size,cmd,item):
                 
-         template = {'offset': offset,'data': data ,'size' :size ,'cmd' : cmd}
+         template = {'offset': offset,'data': data ,'size' :size ,'cmd' : cmd, 'item' : item}
    
          self.JsonPacket = self.JsonPacket ,template
                            
          handle_string = str( str(self.JsonPacket).replace('(','') ).replace(')','')   #dict to string
                         
          
-         self.json_format_string = json.dumps(eval(handle_string),indent=32)   #eval(handle_string) [string to dict] 
+         self.json_format_string = json.dumps(eval(handle_string),indent=32)   #eval(handle_string)->[string to dict] 
          
     #     list_t = json.loads(json.dumps(eval(handle_string),indent=32))  #json to list
     def record_number_cmd(self,_jsonformatdata):
-        print _jsonformatdata    # debug  out the json format string data
+     #   print _jsonformatdata    # debug  out the json format string data
         
         list_t = json.loads(json.dumps(eval(_jsonformatdata),indent=32))  #json to list 
         
+        for index in range(len(list_t)):     #for index in range(len(list_t)):
+            time = 0 
+            if list_t[index]['item'] == 'invaild' :
+                ###
+                time = time + 1 
+                for next in range(index+1,len(list_t)) :
+                    if list_t[index]['cmd'] == list_t[next]['cmd'] and list_t[index]['offset'] == list_t[next]['offset']:
+                            # print list_t[index]    #debug add
+                             list_t[next]['item'] = 'vaild'
+                             time = time +1
+                     
+                    else :
+                        pass
+                list_t[index]['item'] = str(time)
+                
+            else :
+                pass 
+            
+       
+        self.record_json_format_data = json.dumps (list_t,indent=32)    #after handle the source json format data,then put the record data to record_json_format_data
+        
+        print self.record_json_format_data
+           
+        
+        '''
         print list_t        #debug out list data
         
         dict_t = list_t[1]
@@ -118,13 +144,22 @@ class HandleLogClass:
         print dict_t       #debug out dict data
 
         print dict_t['size']        
-             
-        
+        '''     
+    def jsondata_to_file(self):     #put the json data to logjson.log file
+         jsonfile = open('logjson.json','w')
+         jsonfile.write(self.record_json_format_data)
+         jsonfile.close()
+         
+         
+         
 
 Loghandle = HandleLogClass() 
 
 Loghandle.get_key_value()
 
-
+#print Loghandle.json_format_string
 Loghandle.record_number_cmd(Loghandle.json_format_string)
+Loghandle.jsondata_to_file()
+
+
 
